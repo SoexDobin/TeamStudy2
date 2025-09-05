7307360746a530366979ca8e54602386a2d83551
LeeSunJae <ryugakusei3600@gmail.com>
2025-09-05 21:59:15 +0900

    feat: Player & Monster header
    플레이어 및 몬스터 클래스 추가 및 헤더파일 작성

--- Jusin158_Team1_Study/CPlayer.h
+++ Jusin158_Team1_Study/CPlayer.h
@@ -4,11 +4,15 @@
 
 class CPlayer : public CObject
 {
-public :
+public:
 	CPlayer();
 	virtual ~CPlayer();
 
-public :
-	void Intialize() 
-};
+public:
+	void	Initialize()		override;
+	int		Update()			override;
+	void	LateUpdate()		override;
+	void	Render(HDC _hDC)	override;
+	void	Release()			override;
 
+};
