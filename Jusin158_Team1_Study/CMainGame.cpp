75bef21794ca71a7a8ba33972bfe4e00a55e7473
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-09-05 19:53:46 +0900

    feat : init project

---
+++ Jusin158_Team1_Study/CMainGame.cpp
@@ -0,0 +1,80 @@
+﻿#include "pch.h"
+#include "CMainGame.h"
+#include "CObject.h"
+
+CMainGame::CMainGame() : m_hDC(nullptr)
+{
+}
+
+CMainGame::~CMainGame()
+{
+}
+
+void CMainGame::Initialize()
+{
+	m_hDC = GetDC(g_hWnd);
+
+	{ // 더블 버퍼링
+		GetClientRect(g_hWnd, &m_tRect);
+		m_hDCBack = CreateCompatibleDC(m_hDC);
+		m_bmpBack = CreateCompatibleBitmap(m_hDC, m_tRect.right, m_tRect.bottom);
+		HBITMAP prev = (HBITMAP)::SelectObject(m_hDCBack, m_bmpBack);
+		DeleteObject(prev);
+	}
+
+}
+
+void CMainGame::Update()
+{
+	bool bIsDestroy(false);
+	for (int i = 0; i < OBJ_END; ++i)
+	{
+		for (auto iter = m_ObjectList[i].begin(); iter != m_ObjectList[i].end();)
+		{
+			bIsDestroy = (*iter)->Update();
+
+			if (bIsDestroy)
+			{
+				SafeDelete<CObject*>((*iter));
+				iter = m_ObjectList[i].erase(iter);
+			}
+			else
+				++iter;
+		}
+	}
+}
+
+void CMainGame::LateUpdate()
+{
+	for (auto& list : m_ObjectList)
+		for (auto& obj : list)	
+			obj->LateUpdate();
+}
+
+void CMainGame::Render()
+{
+	{ // 기존 Rectangle을 그려서 깜빡임 최소화 한걸 더블 버퍼링으로 바꿈
+		//Rectangle(m_hDC, 0, 0, WINCX, WINCY);
+		BitBlt(m_hDC, 0, 0, m_tRect.right, m_tRect.bottom, m_hDCBack, 0, 0, SRCCOPY);
+		PatBlt(m_hDCBack, 0, 0, m_tRect.right, m_tRect.bottom, WHITENESS);
+	}
+
+	for (auto& list : m_ObjectList)
+		for (auto& obj : list)
+			obj->Render(m_hDC);
+
+	// 여기서 충돌검사를 실행
+}
+
+void CMainGame::Release()
+{
+	for (int i = 0; i < OBJ_END; ++i)
+		for_each(m_ObjectList[i].begin(), m_ObjectList[i].end()
+			, [](CObject* _p) -> void {
+				if (_p)
+				{
+					delete _p;
+					_p = nullptr;
+				}
+			});
+}
---
+++ Jusin158_Team1_Study/Jusin158_Team1_Study.cpp
@@ -0,0 +1,170 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "Jusin158_Team1_Study.h"
+#include "CMainGame.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HWND g_hWnd;
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_JUSIN158TEAM1STUDY, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_JUSIN158TEAM1STUDY));
+
+    MSG msg;
+    msg.message = WM_NULL;
+    ULONGLONG ullTime = GetTickCount64();
+
+    CMainGame GAME;
+    GAME.Initialize();
+    
+    while (1)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            if (ullTime + 10 < GetTickCount64())
+            {
+                GAME.Update();
+                GAME.LateUpdate();
+                GAME.Render();
+
+                ullTime = GetTickCount64();
+            }
+        }
+       
+    }
+    GAME.Release();
+    return (int) msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_JUSIN158TEAM1STUDY));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;//MAKEINTRESOURCEW(IDC_JUSIN158TEAM1STUDY);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+   RECT rcWindow = { 0, 0, WINCX, WINCY };
+   AdjustWindowRect(&rcWindow, WS_OVERLAPPEDWINDOW, FALSE);
+   HWND hWnd = CreateWindowW(szWindowClass
+                            , szTitle
+                            , WS_OVERLAPPEDWINDOW
+                            , CW_USEDEFAULT, 0
+                            , rcWindow.right - rcWindow.left
+                            , rcWindow.bottom - rcWindow.top
+                            , nullptr
+                            , nullptr
+                            , hInstance
+                            , nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
