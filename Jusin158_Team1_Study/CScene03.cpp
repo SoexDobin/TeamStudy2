4eb113f3ec611b68f72b805a8a164497ed77728a
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-09-09 13:04:18 +0900

    fix : add release

--- Jusin158_Team1_Study/CScene03.cpp
+++ Jusin158_Team1_Study/CScene03.cpp
@@ -12,13 +12,13 @@ CScene03::CScene03() : m_pPlayer(nullptr), m_pBoss(nullptr)
 }
 CScene03::~CScene03()
 {
-
+	Release();
 }
 void CScene03::Initialize()
 {
 	CLineManager::GetInstance()->LoadDataThree();
 	m_pPlayer = CObjectManager::GetInstance()->GetPlayer()->back();
-
+	m_pPlayer->SetPivot(Vector2(500, 300));
 	CObjectManager::GetInstance()->AddObject(MONSTER, CAbstractFactory<CMonster>::Create(2700.f, 300.f));
 	m_MonsterList.push_back(CObjectManager::GetInstance()->GetMonsterList()->back());
 	m_pBoss = CObjectManager::GetInstance()->GetMonsterList()->back();
